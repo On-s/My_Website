a4d37f0dd610987bbc68d32fd3cee77ab97b5dae
On-s <dhstjdgns42@gmail.com>
2021-08-09 16:35:16 +0900

    버블 수정

--- code/main/js/bubble.js
+++ code/main/js/bubble.js
@@ -8,14 +8,26 @@ const bubbleCon = document.querySelector('.bubble_con');;
 let bNum = 20;
 
 
+
+
 for (let i = 0; i < bNum; i++) {
-  let bubble = document.createElement('div');
-  bubble.classList.add('bubble');
-  bubbleattr = bubbleCon.getElementsByClassName('.bubble')[i];
-  bubble.style.left = `${Math.floor(Math.random()*81)+10}%`;
-  bubble.style.animation = `bubble-movement ${Math.floor(Math.random()*10)+5}s infinite ease-in-out ${Math.floor(Math.random()*300)}ms`;
-  console.log(document.documentElement.style.setProperty('--my-variable-name', (Math.floor(Math.random()*5)+3)+'rem'));
-  bubbleCon.appendChild(bubble);
+  if (i < bNum/2) {
+    let bubble = document.createElement('div');
+    bubble.classList.add('bubble');
+    bubbleattr = bubbleCon.getElementsByClassName('.bubble')[i];
+    bubble.style.left = `${Math.floor(Math.random()*96)}%`;
+    bubble.style.animation = `bubble-movement-small ${(Math.random()*15)+6}s infinite ease-in-out 0ms`;
+    bubble.style.opacity = `${(Math.floor(Math.random()*9)+5)/10}`
+    bubbleCon.appendChild(bubble);
+  }else {
+    let bubble = document.createElement('div');
+    bubble.classList.add('bubble');
+    bubbleattr = bubbleCon.getElementsByClassName('.bubble')[i];
+    bubble.style.left = `${Math.floor(Math.random()*96)}%`;
+    bubble.style.animation = `bubble-movement-large ${(Math.random()*15)+9}s infinite ease-in-out ${Math.floor(Math.random()*500)}ms`;
+    bubble.style.opacity = `${(Math.floor(Math.random()*9)+5)/10}`
+    bubbleCon.appendChild(bubble);
+  }
 }
 
 
